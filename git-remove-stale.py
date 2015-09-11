1bfcf8e3b123723e6cadeab0c1f2826f0c9d3095
Maks Nemisj <maks@nemisj.com>
2015-09-11 18:42:53 +0200

    py: s/'/"/g

--- git-remove-stale.py
+++ git-remove-stale.py
@@ -5,8 +5,8 @@ import re
 import argparse
 
 parser = argparse.ArgumentParser(description="Remove local branches, which are no longer available in the remote")
-parser.add_argument("--do-it", action='store_true', help="Remove branches")
-parser.add_argument("--force", action='store_true', help="Force deletion")
+parser.add_argument("--do-it", action="store_true", help="Remove branches")
+parser.add_argument("--force", action="store_true", help="Force deletion")
 parser.add_argument("--remote", default="origin", help="Remote name")
 args = parser.parse_args()
 
@@ -53,9 +53,9 @@ def remove_branches(branches):
     if (args.do_it):
       print "Removing %s" % branch_name
       if argv.force:
-        deleteFlag = '-D'
+        deleteFlag = "-D"
       else:
-        deleteFlag = '-d'
+        deleteFlag = "-d"
 
       return_code = subprocess.call(["git", "branch", deleteFlag, branch_name]);
       if return_code != 0:
