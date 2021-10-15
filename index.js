1f702b57602746bacb4d612211960148f436d498
Maks Nemisj <nemisj@nemisj.com>
2021-10-15 21:04:12 +0200

    Rewrite using async/await, arrow functions and const

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var FindStale = require('./lib/find-stale.js');
-var utils = require('./lib/utils.js');
+const FindStale = require('./lib/find-stale.js');
+const utils = require('./lib/utils.js');
 
-var argv = require('minimist')(process.argv, {
+const argv = require('minimist')(process.argv, {
   string: 'remote',
   boolean: ['prune', 'force'],
   alias: {p: "prune", f: "force", r: "remote"},
@@ -20,14 +20,14 @@ if (hasInvalidParams) {
   console.info('Usage: git removed-branches [-p|--prune] [-f|--force] [-r|--remote <remote>]');
 } else {
   // check for git repository
-  var exec = utils.asyncExec(['git', 'rev-parse', '--show-toplevel']);
-  var obj = new FindStale({
+  const exec = utils.asyncExec(['git', 'rev-parse', '--show-toplevel']);
+  const obj = new FindStale({
     remove: argv.prune,
     force: argv.force,
     remote: argv.remote
   });
   
-  exec(function (err, stdout, stderr) {
+  exec((err, stdout, stderr) => {
     if (err) {
       console.error(err.message);
       return;
